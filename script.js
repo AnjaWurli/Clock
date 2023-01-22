209cfca2e0941a37998ce06304d2dd71dd672e7d
Anja Wurlitzer <anja.wurlitzer.coding@gmail.com>
2023-01-22 15:30:00 +0100

    refactor: remove global variables

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-const digital = document.querySelector("p");
-
-const hour = document.querySelector("#hourpointer");
-const minute = document.querySelector("#minpointer");
-const second = document.querySelector("#secpointer");
-
-//set init digital time:
-digital.innerText = newTime();
-
-setInterval(myCallback, 1000);
-
-function myCallback() {
-  digital.innerText = newTime();
-}
+setInterval(() => (newTime(), 1000));
 
 function newTime() {
   const d = new Date();
-  //Jan 22 2023 02:08:03
 
   //return numbers smaller than 10 in 2 digits:
   const zero = (n) => (n < 10 ? "0" + n : n);
@@ -24,15 +10,21 @@ function newTime() {
   let min = zero(d.getMinutes());
   let s = zero(d.getSeconds());
 
-  //pointers
-  hour.style.setProperty("rotate", `${h * (360 / 12)}deg`);
-  minute.style.setProperty("rotate", `${min * (360 / 60)}deg`);
-  second.style.setProperty("rotate", `${s * (360 / 60)}deg`);
+  //pointers of the classic clock
+  document
+    .querySelector("#hourpointer")
+    .style.setProperty("rotate", `${h * (360 / 12)}deg`);
+  document
+    .querySelector("#minpointer")
+    .style.setProperty("rotate", `${min * (360 / 60)}deg`);
+  document
+    .querySelector("#secpointer")
+    .style.setProperty("rotate", `${s * (360 / 60)}deg`);
 
   //blinking colon:
   if (s % 2 === 0) {
-    return h + ":" + min + ":" + s;
+    document.querySelector("p").innerText = h + ":" + min + ":" + s;
   } else {
-    return h + " " + min + " " + s;
+    document.querySelector("p").innerText = h + " " + min + " " + s;
   }
 }
